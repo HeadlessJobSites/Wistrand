e920e9caafd257589df1eb7dc1704f037ea45d82
henrikmoberg <102950716+henrikmoberg@users.noreply.github.com>
2024-10-03 14:31:38 +0200

    Update page-title.js

--- js/page-title.js
+++ js/page-title.js
@@ -1,14 +1,9 @@
-    // Function to dynamically set the URLs for social media sharing
-    function setSocialMediaShareUrls() {
-        var facebookShareLink = document.getElementById('facebook-share-link');
-        var twitterShareLink = document.getElementById('twitter-share-link');
-        var linkedinShareLink = document.getElementById('linkedin-share-link');
-        var currentUrl = encodeURIComponent(window.location.href);
-
-        facebookShareLink.href = 'https://www.facebook.com/sharer/sharer.php?u=' + currentUrl;
-        twitterShareLink.href = 'https://twitter.com/intent/tweet?url=' + currentUrl;
-        linkedinShareLink.href = 'https://www.linkedin.com/shareArticle?url=' + currentUrl;
+<script>
+document.addEventListener('DOMContentLoaded', function() {
+    // Get the content of the position-title element
+    var positionTitle = document.getElementById('position-title').textContent.trim();
+    if (positionTitle) {
+        document.title = positionTitle; // Set the page title to the position title
     }
-
-    // Call the function to set social media share URLs
-    setSocialMediaShareUrls();
+});
+</script>
