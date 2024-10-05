e0cb3a0b5bdc274b3cf2676e672296dc8d73de9a
henrikmoberg <102950716+henrikmoberg@users.noreply.github.com>
2024-10-05 22:22:00 +0200

    Update page-title.js

--- js/page-title.js
+++ js/page-title.js
@@ -1,1 +1,11 @@
+<script>
+document.addEventListener('DOMContentLoaded', function() {
+    // Get the content of the position-title element
+    var positionTitle = document.getElementById('position-title').textContent.trim();
 
+    // Only update the title if it's still set to the default
+    if (positionTitle && document.title === 'Jobbannons') {
+        document.title = positionTitle; // Set the page title to the position title
+    }
+});
+</script>
