eefedd6bf7710ec3fa0a63c999d06021d55fe1aa
henrikmoberg <102950716+henrikmoberg@users.noreply.github.com>
2025-01-20 10:36:11 +0100

    Update translations.js

--- js/translations.js
+++ js/translations.js
@@ -1,33 +1,42 @@
 document.addEventListener('DOMContentLoaded', function () {
-  i18next
-    .use(i18nextHttpBackend)  // Use the backend plugin available globally
-    .init({
-      lng: 'en', // Default language
-      fallbackLng: 'en',
-      debug: true,
-      backend: {
-        loadPath: './locales/{{lng}}/translation.json'  // Path to the translation files
-      }
-    }, function(err, t) {
-      // Initialize your application after translations have been loaded
-      updateContent();
+  console.log('Checking i18nextHttpBackend availability:', typeof i18nextHttpBackend);
+  if (typeof i18nextHttpBackend === 'undefined') {
+    console.error('i18nextHttpBackend is not defined.');
+  } else {
+    i18next
+      .use(i18nextHttpBackend)  // Use the backend plugin available globally
+      .init({
+        lng: 'en', // Default language
+        fallbackLng: 'en',
+        debug: true,
+        backend: {
+          loadPath: './locales/{{lng}}/translation.json'  // Path to the translation files
+        }
+      }, function(err, t) {
+        if (err) {
+          console.error('Something went wrong during the i18next initialization:', err);
+          return;
+        }
+        // Initialize your application after translations have been loaded
+        updateContent();
+      });
+
+    function updateContent() {
+      document.getElementById('lang_herotext').innerHTML = i18next.t('lang_herotext');
+    }
+
+    function changeLanguage(lng) {
+      i18next.changeLanguage(lng, () => {
+        updateContent();
+      });
+    }
+
+    // Add language change event listeners if needed
+    document.getElementById('switch-to-en').addEventListener('click', function () {
+      changeLanguage('en');
     });
-
-  function updateContent() {
-    document.getElementById('lang_herotext').innerHTML = i18next.t('lang_herotext');
-  }
-
-  function changeLanguage(lng) {
-    i18next.changeLanguage(lng, () => {
-      updateContent();
+    document.getElementById('switch-to-sv').addEventListener('click', function () {
+      changeLanguage('sv');
     });
   }
-
-  // Add language change event listeners if needed
-  document.getElementById('switch-to-en').addEventListener('click', function () {
-    changeLanguage('en');
-  });
-  document.getElementById('switch-to-sv').addEventListener('click', function () {
-    changeLanguage('sv');
-  });
 });
