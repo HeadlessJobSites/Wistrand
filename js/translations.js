1e5ba2f9722c46559e0c262b602164e337ea0d78
henrikmoberg <102950716+henrikmoberg@users.noreply.github.com>
2025-01-21 10:53:16 +0100

    Update translations.js

--- js/translations.js
+++ js/translations.js
@@ -29,7 +29,7 @@ document.addEventListener('DOMContentLoaded', function () {
       const elementsToUpdate = [
         { id: 'lang_herotext', key: 'lang_herotext' },
         { id: 'lang_studenthero', key: 'lang_studenthero' },
-        { id: 'lang_herobtn_default', key: 'hero_button_default' },
+        { id: 'lang_herobtn_standard', key: 'lang_herobtn_standard' },
         { id: 'company_header', key: 'company_section.header' },
         { id: 'company_description', key: 'company_section.description' },
         { id: 'values_header', key: 'values_section.header' },
